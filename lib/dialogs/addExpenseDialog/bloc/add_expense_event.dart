af3d4c9e04efa9f47bd0c8cae4be62a7f662ef0f
sakshamnegi07 <sakshamnegi@supersixsports.com>
2024-08-23 12:23:38 +0530

    Error handling in dialogs

---
+++ lib/dialogs/addExpenseDialog/bloc/add_expense_event.dart
@@ -0,0 +1,4 @@
+part of 'add_expense_bloc.dart';
+
+@immutable
+sealed class AddExpenseEvent {}
---
+++ lib/dialogs/addExpenseDialog/bloc/add_expense_state.dart
@@ -0,0 +1,6 @@
+part of 'add_expense_bloc.dart';
+
+@immutable
+sealed class AddExpenseState {}
+
+final class AddExpenseInitial extends AddExpenseState {}
